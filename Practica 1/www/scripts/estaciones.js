b380787255b2b8a44c29341e8339f86f97d4302e
Juliyo <julio17795@hotmail.com>
2015-10-02 22:55:28 +0200

    Añadida la funcionalidad de modificar datos de la tabla

--- Practica 1/www/scripts/estaciones.js
+++ Practica 1/www/scripts/estaciones.js
@@ -2,7 +2,7 @@
 var dataBase = null;
 var active = null;
 var estaciones = [];
-
+//Iniciamos la base de datos y llamamos a cargar estaciones
 function startBD() {
     dataBase = indexedDB.open("monitor", "1");
     dataBase.onupgradeneeded = function (e) {
@@ -13,6 +13,7 @@ function startBD() {
         cargarEstaciones();
     };
 }
+//Cargamos las estaciones abriendo una transaccion con la base de datos y guardamos los resultados en elements
 function cargarEstaciones() {
     estaciones = [];
     active = dataBase.result;
@@ -32,6 +33,7 @@ function cargarEstaciones() {
         result.continue();
 
     };
+    //Cuando se complete la transaccion nos guardaremos las estaciones leidas en un array
     data.oncomplete = function () {
 
         for (var key in elements) {
@@ -40,7 +42,7 @@ function cargarEstaciones() {
         }
 
         elements = [];
-        
+        //LLamamos a este metodo que cargara en el html los datos leidos
         generateHtml();
     };
     
@@ -49,9 +51,9 @@ function cargarEstaciones() {
 function generateHtml() {
 
     var html = '';
-
-    $('#tablaEstaciones tbody tr').remove();
-
+    //Vaciamos la tabla
+    $('#tablaEstaciones tbody').empty();
+    //LLenamos la tabla
     for (var i = 0; i < estaciones.length; i++) {
 
         html = html + '<tr><td><input type="checkbox" ></td><th scope="row">' + estaciones[i].identificadorLector + '</th><td>' + estaciones[i].latitud + '</td><td>' + estaciones[i].longitud + '</td></tr>';
@@ -73,9 +75,73 @@ function generateHtml() {
 function fade(objeto) {
     $(objeto).addClass("load");
 }
-document.getElementsByClassName('btn-success').onclick = function (e) {
-
-    $('input[type=checkbox]').each(function (fila, obj) {
-        if($(this).is(":cheked"))
+//Cuando el usuario pulse en el boton modificar
+$("#modificar").click(function (e) {
+    var encontrado = false;
+    var f;
+    //Vemos que fila esta marcada con check
+    $("input[type=checkbox]").each(function (fila, obj) {
+        if ($(this).is(":checked") && encontrado==false) {
+            f=fila;
+            encontrado = true;
+        }
     });
-};
+    $("#tablaEstaciones tbody tr").each(function (fila, obj) {
+        if (fila == f) {
+            var html = '<td><input type="checkbox" checked></td><th scope="row"><input class="form-control" type="text"></th><td><input class="form-control" type="text"></td><td><input class="form-control" type="text"></td>';
+            $(this).empty();
+            $(this).append(html);
+            $(".crud2").css({ "display": "" });
+            $(".crud").css({ "display": "none" });
+            $("#aceptar").click(function (e) {
+                modificar(fila);
+            });
+            $("#cancelar").click(function (e) {
+                $(".crud2").css({ "display": "none" });
+                $(".crud").css({ "display": "" });
+                generateHtml();
+            });
+            
+            $("input[type=checkbox]").each(function () {
+                    $(this).attr("disabled", true);
+            });
+            
+        }
+    });
+});
+function modificar(fila) {
+   
+    active = dataBase.result;
+    var data = active.transaction(["EstacionesLectoras"], "readwrite");
+    var object = data.objectStore("EstacionesLectoras");
+
+    object.get(estaciones[fila].identificadorLector).onsuccess = function (e) {
+        console.log("store.get", fila);
+        var data = e.target.result;
+        if (!data) {
+            console.log("nothing matched.");
+            return;
+        }
+        // modify 'name' to upperCase
+        var datos = [];
+        $(".form-control").each(function (fila, obj) {
+            datos[fila] = $(this).val();
+        });
+        data.identificadorLector = datos[0];
+        data.latitud = datos[1];
+        data.longitud = datos[2];
+        var request = object.put(data);
+
+        request.onsuccess = function (e) {
+            console.log("put success!");
+        };
+
+        request.onerror = function (e) {
+            console.log("put error!");
+        };
+    };
+    $(".crud2").css({ "display": "none" });
+    $(".crud").css({ "display": "" });
+    cargarEstaciones();
+    
+}
